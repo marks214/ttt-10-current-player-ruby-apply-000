91c037fada3a9b1e72fb2122b54f8563ca186704
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-30 00:15:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 def turn_count(board)
+<<<<<<< HEAD
     counter = 0
     turn_taken = 0
 
@@ -20,4 +21,14 @@ def current_player(board)
   else
     return "O"
   end
+=======
+  counter = 0
+
+  board.each do |turn|
+    if board[counter] == "X" || board[counter] == "O"
+      counter += 1
+    end
+  end
+  return counter
+>>>>>>> 4c9a02f4189927afef717107ef8c8fc253be3f36
 end
